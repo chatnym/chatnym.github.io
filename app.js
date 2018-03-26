d6a2554229360f82414479d15ccb9f88c9302160
Arafat Husayn <arafat@tuta.io>
2018-03-26 18:08:03 +0600

    add more ice servers

--- app.js
+++ app.js
@@ -80,7 +80,33 @@ let previouslyPressedKey = 0;
 
     const p = new SimplePeer({
       initiator: isInitiator,
-      trickle: false
+      reconnectTimer: 100,
+      iceTransportPolicy: 'relay',
+      trickle: false,
+      config: {
+        iceServers: [
+          {urls:'stun:stun01.sipphone.com'},
+          {urls:'stun:stun.ekiga.net'},
+          {urls:'stun:stun.fwdnet.net'},
+          {urls:'stun:stun.ideasip.com'},
+          {urls:'stun:stun.iptel.org'},
+          {urls:'stun:stun.rixtelecom.se'},
+          {urls:'stun:stun.schlund.de'},
+          {urls:'stun:stun.l.google.com:19302'},
+          {urls:'stun:stun1.l.google.com:19302'},
+          {urls:'stun:stun2.l.google.com:19302'},
+          {urls:'stun:stun3.l.google.com:19302'},
+          {urls:'stun:stun4.l.google.com:19302'},
+          {urls:'stun:stunserver.org'},
+          {urls:'stun:stun.softjoys.com'},
+          {urls:'stun:stun.voiparound.com'},
+          {urls:'stun:stun.voipbuster.com'},
+          {urls:'stun:stun.voipstunt.com'},
+          {urls:'stun:stun.voxgratia.org'},
+          {urls:'stun:stun.xten.com'},
+          {urls: 'stun:global.stun.twilio.com:3478?transport=udp'}
+        ]
+      }
     });
 
     p.on('error', function (err) { console.error('error', err) });
