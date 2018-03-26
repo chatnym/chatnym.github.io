a15f04c9a5df555f229c1ba65d7c60dd3673ce49
Arafat Husayn <arafat@tuta.io>
2018-03-26 17:53:41 +0600

    trickle false

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ let previouslyPressedKey = 0;
 
     const p = new SimplePeer({
       initiator: isInitiator,
-      trickle: true
+      trickle: false
     });
 
     p.on('error', function (err) { console.error('error', err) });
